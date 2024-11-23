4c09bb9389fd17b9f8178f33fb76c89ede0713ef
Sanstar12 <94879410+Sanstar12@users.noreply.github.com>
2024-11-24 00:24:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ def progress(current, total, message, type):
 # start command
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**\n\n{USAGE}",
-	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/VJ_Botz")]]), reply_to_message_id=message.id)
+	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **Welcome to Forword master, This bot is designed to help you access and forward restricted content from Telegram channels, includig any content__**\n\n{USAGE}",
+	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 OUR Channel", url="https://t.me/unlockedEdu")]]), reply_to_message_id=message.id)
 
 
 @bot.on_message(filters.text)
